4f2d5fcf1eb6ac33dae91dd1bf4e46eb63229ae0
Muhammad Zaki Amanullah <13521146@std.stei.itb.ac.id>
2024-05-06 11:08:20 +0700

    docs: readme.md

--- app/src/main/java/com/queentylion/gestra/ui/screens/settings/Settings.kt
+++ app/src/main/java/com/queentylion/gestra/ui/screens/settings/Settings.kt
@@ -3,7 +3,6 @@ package com.queentylion.gestra.ui.screens.settings
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
